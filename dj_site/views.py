4afef48495fc1429c89278fbf7a4551a33b870c6
Oksana Slusarenko <oksana.slu@gmail.com>
2010-08-03 20:08:38 +0300

    first steps for rendering class data to template

--- dj_site/views.py
+++ dj_site/views.py
@@ -5,17 +5,18 @@ from django.template import RequestContext
 
 
 def index(request):
-    return render_to_response('index.html', {}, 
+    napr_xt_classes = [] #XtClass.objects.filter(nclasstype=1)[:11]
+    return render_to_response('index.html', {"napr_xt_classes": napr_xt_classes}, 
                               context_instance=RequestContext(request))
                               
                               
-def xtreport_list(request):
+def xtreport_list(request, part):
     return render_to_response('repo_list.html', {}, 
                               context_instance=RequestContext(request))
                               
                               
                               
-def xtreport_item(request, slug):
+def xtreport_item(request, part, slug):
     return render_to_response('repo.html', {}, 
                               context_instance=RequestContext(request))
                               
