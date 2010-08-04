0b6c28aa9aeac46682f71083ebcb54bf40b1b18a
Oksana Slusarenko <oksana.slu@gmail.com>
2010-08-04 19:07:13 +0300

    added app xtclass

---
+++ dj_site/xtclass/admin.py
@@ -0,0 +1,7 @@
+from dj_site.xtclass.models import XtC2C, XtClass, XtClasstype
+from django.contrib import admin
+
+
+admin.site.register(XtC2C)
+admin.site.register(XtClass)
+admin.site.register(XtClasstype)
---
+++ dj_site/xtclass/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+       
+             
+        
+class XtClasstype(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nme = models.CharField(max_length=66)
+    xtct_desc = models.CharField(max_length=300, blank=True)
+    class Meta:
+        db_table = u'xt_classtype'
+       
+       
+
+class XtClass(models.Model):
+    nid = models.IntegerField(primary_key=True)
+    vname = models.CharField(max_length=264)
+    vcode = models.CharField(max_length=264)
+    xtcdescription = models.CharField(max_length=240)
+    xtclasstype = models.ForeignKey(XtClasstype, db_column='nclasstype')
+    nshablon = models.IntegerField()
+    xtcconst = models.CharField(max_length=90)
+    class_order = models.IntegerField()
+    
+    class Meta:
+        db_table = u'xt_class'
+        
+        
+        
+class XtC2C(models.Model):
+    parent = models.ForeignKey(XtClass, db_column='c2c_parent', related_name='parent')
+    child = models.ForeignKey(XtClass, db_column='c2c_child', related_name='child')
+    
+    class Meta:
+        db_table = u'xt_c2c'
+        unique_together = (("parent", "child"),)
