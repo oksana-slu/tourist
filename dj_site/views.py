e01441e10d2c0d37fc2704f40e5343f73ebbbbd1
Aleksey Radchenko <dixon.che@gmail.com>
2010-08-05 16:25:52 +0300

    Changes in xtclas-querysets

--- dj_site/views.py
+++ dj_site/views.py
@@ -6,24 +6,28 @@ from dj_site.xtclass.models import XtClass
 
 
 def index(request):
-    geogr_xt_classes = XtClass.objects.filter(xtclasstype__pk=2)[:6]
-    napr_xt_classes = XtClass.objects.filter(xtclasstype__pk=1).order_by('-class_order')[:11]
+    geogr_xt_classes = XtClass.objects.filter(xtclasstype__pk=2,
+                                             child__parent=0).\
+                                             order_by('-class_order')
+    napr_xt_classes = XtClass.objects.filter(xtclasstype__pk=1,
+                                             child__parent=0).\
+                                             order_by('-class_order')
+
     return render_to_response('index.html', {"napr_xt_classes": napr_xt_classes,
-                                            "geogr_xt_classes": geogr_xt_classes}, 
+                                             "geogr_xt_classes": geogr_xt_classes},
                               context_instance=RequestContext(request))
-                              
-                              
+
+
 def xtreport_list(request, part):
-    return render_to_response('repo_list.html', {}, 
+    return render_to_response('repo_list.html', {},
                               context_instance=RequestContext(request))
-                              
-                              
-                              
+
+
 def xtreport_item(request, part, slug):
-    return render_to_response('repo.html', {}, 
+    return render_to_response('repo.html', {},
                               context_instance=RequestContext(request))
-                              
-                              
+
+
 def xtclass(request, slug):
-    return render_to_response('class.html', {}, 
+    return render_to_response('class.html', {},
                               context_instance=RequestContext(request))
