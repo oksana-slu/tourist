4805e41fe19dc843298bb6d7ec3589128e72a88d
Aleksey Radchenko <dixon.che@gmail.com>
2010-08-16 12:17:58 +0300

    change in fix script

--- dj_site/fix_content_types.py
+++ dj_site/fix_content_types.py
@@ -1,4 +1,8 @@
 from dj_site.xtobject.models import XtObject
-XtObject.objects.filter(xtobjecttype__pk__in = (1, 2)).update(content_type=17)
-XtObject.objects.filter(xtobjecttype__pk = 3).update(content_type=15)
-XtObject.objects.filter(xtobjecttype__pk = 5).update(content_type=16)
+from django.contrib.contenttypes.models import ContentType
+topic = ContentType.objects.get(model='xttopic')
+XtObject.objects.filter(xtobjecttype__pk__in = (1, 2)).update(content_type=topic)
+link = ContentType.objects.get(model='xtlink')
+XtObject.objects.filter(xtobjecttype__pk = 3).update(content_type=link)
+news = ContentType.objects.get(model='xtnews')
+XtObject.objects.filter(xtobjecttype__pk = 5).update(content_type=news)
