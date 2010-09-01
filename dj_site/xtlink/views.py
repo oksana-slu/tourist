8a83af0e4ff887fb1aa53f39a9c8ac441aa4b0fb
Oksana Slusarenko <oksana.slu@gmail.com>
2010-09-01 21:17:44 +0300

    changes in the form

--- dj_site/xtlink/views.py
+++ dj_site/xtlink/views.py
@@ -17,6 +17,15 @@ class LinkForm(forms.Form):
 
 
 def edit_link(request):
+    geogr_xt_classes = XtClass.objects.filter(xtclasstype__pk=2,
+                                             childs__parent=0).\
+                                             order_by('-class_order')
+    napr_xt_classes = XtClass.objects.filter(xtclasstype__pk=1,
+                                             childs__parent=0).\
+                                             order_by('-class_order')
+    hrdly_xt_classes = XtClass.objects.filter(xtclasstype__pk=3,
+                                             childs__parent=0).\
+                                             order_by('-class_order')
     if request.method == 'POST':
         form = LinkForm(request.POST)
         if form.is_valid():
@@ -25,5 +34,7 @@ def edit_link(request):
     else:
         form = LinkForm() 
 
-    return render_to_response('edit_link.html', {},
+    return render_to_response('edit_link.html', {"napr_xt_classes": napr_xt_classes,
+                                                 "geogr_xt_classes": geogr_xt_classes,
+                                                 "hrdly_xt_classes": hrdly_xt_classes},
                               context_instance=RequestContext(request, {'form': form}))
